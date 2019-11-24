3f8fb41d13e438950381887e54128ed5befbfd3a
renan <renanalves23@outlook.com>
2019-11-24 16:58:52 -0300

    Commit Inicial Servidor básico criado

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+//Query params = ?teste=1
+//Route params = /users/1
+//Request body = { "name": "Renan", "email": "email@contato.com" }
+
+const users = ['Diego', 'Renan', 'Victor'];
+
+//retorna todos os usuários utiliza Route params
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+//retorna um usuário utiliza Route params
+server.get('/users/:index', (req, res) => {
+    const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+//utiliza Request Body
+server.post('/users', (req, res) => {
+    const { name } = req.body;
+
+    users.push(name);
+
+    return res.json(users);
+});
+
+//utiliza Request Body
+server.put('/users/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    users[index] = name;
+
+    return res.json();
+});
+
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  returnres.json(users); 
+
+});
+server.listen(3003);
